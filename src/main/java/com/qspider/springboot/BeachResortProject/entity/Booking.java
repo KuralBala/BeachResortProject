1b5228b12f6c54b4383a6fdca688dadde48f5158
KuralBala <smartkural@gmail.com>
2024-08-20 09:16:55 +0530

    Beach Resort Project Added

---
+++ src/main/java/com/qspider/springboot/BeachResortProject/entity/Booking.java
@@ -0,0 +1,54 @@
+package com.qspider.springboot.BeachResortProject.entity;
+
+public class Booking {
+	private String id;
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	private String room_status;
+	private String payment_status;
+	
+//	private List<Rooms> rooms;
+//	
+//	private List<Services> services;
+
+	public String getRoom_status() {
+		return room_status;
+	}
+
+	public void setRoom_status(String room_status) {
+		this.room_status = room_status;
+	}
+
+	public String getPayment_status() {
+		return payment_status;
+	}
+
+	public void setPayment_status(String payment_status) {
+		this.payment_status = payment_status;
+	}
+//
+//	public List<Rooms> getRooms() {
+//		return rooms;
+//	}
+//
+//	public void setRooms(List<Rooms> rooms) {
+//		this.rooms = rooms;
+//	}
+//
+//	public List<Services> getServices() {
+//		return services;
+//	}
+//
+//	public void setServices(List<Services> services) {
+//		this.services = services;
+//	}
+
+	
+	
+
+}
---
+++ src/test/java/com/qspider/springboot/BeachResortProject/BeachResortProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qspider.springboot.BeachResortProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeachResortProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
